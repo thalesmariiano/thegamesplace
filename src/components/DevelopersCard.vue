4e40887392aa512efe6ecbb1c7bf0e0c2d5ac190
Thales Mariano <thales.mariano125@gmail.com>
2023-09-18 17:08:58 -0300

    deveveloperscard done

--- src/components/DevelopersCard.vue
+++ src/components/DevelopersCard.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="relative w-72 h-40 shrink-0 overflow-hidden rounded-lg">
 		<div>
-			<img class="w-full h-full" :src="game.image_background">
+			<img class="w-full h-full" :src="dev.image_background">
 		</div>
-		<div class="w-full h-full absolute top-0 left-0">
-			<p class="text-center text-red">Developer</p>
+		<div class="w-full h-full absolute top-0 left-0 flex justify-center items-center text-center bg-neutral-900/75">
+			<p class="text-white text-xl font-bold">{{ dev.name }}</p>
 		</div>
 	</div>
 </template>
