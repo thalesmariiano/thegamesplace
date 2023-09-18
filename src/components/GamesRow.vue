d8ce35515365161ac744790306635723a3699302
Thales Mariano <thales.mariano125@gmail.com>
2023-09-18 15:58:24 -0300

    GamesRow component created

---
+++ src/components/GamesRow.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="flex gap-4 my-5 p-2 scroll-px-5 overflow-x-scroll shadow-gamesRow">
+          <slot />
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'App',
+		components: {
+			
+		},
+		setup(){
+
+		}
+	}
+</script>
