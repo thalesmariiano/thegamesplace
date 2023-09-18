d6cd6b1332e322079b6ffb784052e59754b0c380
Thales Mariano <thales.mariano125@gmail.com>
2023-09-18 16:09:31 -0300

    header updated and styles changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <template>
-	<div class="flex gap-x-16 items-center p-5 bg-neutral-900 text-white shadow-md">
+	<div class="flex justify-between items-center p-5 bg-neutral-900 text-white shadow-md">
 		<h1 class="uppercase font-bold text-2xl text-red-500">The Games Place</h1>
-		<nav class="space-x-3">
-			<RouterLink class="text-lg hover:underline" to="/">Início</RouterLink>
-			<RouterLink class="text-lg hover:underline" to="/games">Jogos</RouterLink>
-			<RouterLink class="text-lg hover:underline" to="/developers">Desenvolvedoras</RouterLink>
+		<nav class="space-x-8 hidden md:inline">
+			<RouterLink class="text-lg font-semibold hover:underline" to="/">Início</RouterLink>
+			<RouterLink class="text-lg font-semibold hover:underline" to="/games">Jogos</RouterLink>
+			<RouterLink class="text-lg font-semibold hover:underline" to="/developers">Desenvolvedoras</RouterLink>
 		</nav>
+
+		<div class="mt-1.5 inline md:hidden">
+			<button class="space-y-1.5">
+				<div class="w-7 h-1 bg-white"></div>
+				<div class="w-7 h-1 bg-white"></div>
+				<div class="w-7 h-1 bg-white"></div>
+			</button>
+		</div>
 	</div>
 </template>
 
