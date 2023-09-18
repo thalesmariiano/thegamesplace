a1e8553352f6aafa46d4c8fc91cfdf57fcf2dbf8
Thales Mariano <thales.mariano125@gmail.com>
2023-09-18 16:28:47 -0300

    DeveloperCard test created

---
+++ src/components/DevelopersCard.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="relative w-72 h-40 shrink-0 overflow-hidden rounded-lg">
+		<div>
+			<img class="w-full h-full" :src="game.image_background">
+		</div>
+		<div class="w-full h-full absolute top-0 left-0">
+			<p class="text-center text-red">Developer</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			dev: {
+				type: Object,
+				default: {}
+			}
+		}
+	}
+	
+</script>
