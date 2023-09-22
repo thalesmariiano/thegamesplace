2e7112b2fac64badc636411d3597ec773e48d4bd
Thales Mariano <thales.mariano125@gmail.com>
2023-09-22 14:49:57 -0300

    gradient added to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex justify-center items-center bg-neutral-900">
+	<div class="flex justify-center items-center bg-gradient-to-t from-neutral-900 to-neutral-800">
 		<div class="py-5 flex flex-col items-center">
 			<h1 class="uppercase font-bold text-2xl text-red-500">The Games Place</h1>
 			<img class="w-36 mt-3" src="/thalesdeveloper-logo.png">
