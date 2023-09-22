8ace7882f38b8acc612bdc9b2388a0c6ae51c0b2
Thales Mariano <thales.mariano125@gmail.com>
2023-09-22 16:31:18 -0300

    styles changes

--- src/components/GamesDetail.vue
+++ src/components/GamesDetail.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="w-full h-[30rem] relative bg-top bg-center" :style="{backgroundImage: 'url(' + game.background_image + ')'}">
+	<div class="w-full h-96 relative bg-top bg-center" :style="{backgroundImage: 'url(' + game.background_image + ')'}">
 
-		<div class="absolute top-0 left-0 w-full h-full p-5 text-white bg-gradient-to-t from-neutral-800 to-neutral-900/[.7] flex items-end">
+		<div class="absolute top-0 left-0 w-full h-full p-5 text-white bg-gradient-to-t from-neutral-800 to-neutral-900/[.8] flex items-end">
 			<div>
 				<h1
 					class="font-bold text-white text-4xl">
