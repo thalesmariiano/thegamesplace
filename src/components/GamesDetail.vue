66e3032bf5ee6b58f6a1593d49c497be5a89e79a
Thales Mariano <thales.mariano125@gmail.com>
2023-09-22 14:37:55 -0300

    layout update

--- src/components/GamesDetail.vue
+++ src/components/GamesDetail.vue
@@ -1,15 +1,22 @@
 <template>
-	<div class="w-full h-screen relative bg-top bg-center" :style="{backgroundImage: 'url(' + game.background_image + ')'}">
-		<div class="absolute top-0 left-0 w-full h-full p-5 text-white bg-gradient-to-t from-neutral-900 to-neutral-900/75">
+	<div class="w-full h-[30rem] relative bg-top bg-center" :style="{backgroundImage: 'url(' + game.background_image + ')'}">
 
-			<h1 class="font-bold text-white text-4xl">{{ game.name }}</h1>
-			<p>{{ new Date(game.released).getFullYear() }}</p>
-			<p
-				v-for="developer in game.developers"
-				:key="developer.id"
-			>
-				{{ developer.name }}
-			</p>
+		<div class="absolute top-0 left-0 w-full h-full p-5 text-white bg-gradient-to-t from-neutral-800 to-neutral-900/[.7] flex items-end">
+			<div>
+				<h1
+					class="font-bold text-white text-4xl">
+					{{ game.name }}
+				</h1>
+				<p
+					v-show="game.released">
+					{{ new Date(game.released).getFullYear() }}
+				</p>
+				<p
+					v-for="developer in game.developers"
+					:key="developer.id">
+					{{ developer.name }}
+				</p>
+			</div>
 
 		</div>
 	</div>
